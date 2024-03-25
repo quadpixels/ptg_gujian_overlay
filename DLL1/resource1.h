513d9a79290d1da2e3aaeb925d3b12b8e09a1b24
nitroglycerine <suichen6@gmail.com>
2024-03-25 00:18:00 -0700

    Add resource1.h

---
+++ DLL1/resource1.h
@@ -0,0 +1,21 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDR_RCDATA1                     106
+#define IDR_RCDATA2                     107
+#define IDR_RCDATA3                     109
+#define IDR_RCDATA4                     110
+#define IDR_RCDATA5                     111
+#define IDR_FONT1                       114
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
